9778756779e4d93b2ed581717157f701221d8988
iori <feltoan0124@gmail.com>
2023-10-02 09:48:49 +0900

    不要ファイルの削除

--- Othello/src/main/java/model/OthelloLogic.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class OthelloLogic {
-	public static void execute(int turn,String position){
-		if(turn == 0) {
-		}
-	}
-}
--- Othello/src/main/java/model/SettingLogic.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class SettingLogic {
-	SettingLogic(String name , Boolean isFirst,boolean isComputer){
-
-	}
-}
